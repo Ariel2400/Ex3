001014718c15cd52e232fce98c94c35257115660
Tal Ben-zvi <talbz03@gmail.com>
2020-09-03 18:16:05 +0300

    fixed minor bugs in 'hash_calculator.cpp'

--- src/hash/hash_calculator.cpp
+++ src/hash/hash_calculator.cpp
@@ -5,7 +5,7 @@
 bool HashCalculator::encode(std::string path, uint32_t *remainder) {
   std::ifstream file{path, std::ios::binary};
   if (!file) {
-    std::cerr << "Can't open file" << path << std::endl;
+    std::cerr << "Can't open file " << path << std::endl;
     return false;
   } else {
     std::string buffer((std::istreambuf_iterator<char>(file)),
@@ -32,10 +32,10 @@ bool HashCalculator::write(std::string path, uint32_t remainder) {
 
 bool HashCalculator::crc32(std::string input, std::string output) {
   uint32_t remainder;
-  if (!encode(input, &remainder)) {
+  if (!encode("../" + input, &remainder)) {
     return false;
   }
-  if (!write(output, remainder)) {
+  if (!write("../" + output, remainder)) {
     return false;
   }
   return true;
