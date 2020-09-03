29e069ddf85d2b0c5c7b24aecfd4c5ff6355eac6
Tal Ben-zvi <talbz03@gmail.com>
2020-09-03 18:15:03 +0300

    tweaked image files

--- src/images/image_editor.cpp
+++ src/images/image_editor.cpp
@@ -5,21 +5,21 @@
 bool ImageEditor::rotate(const std::string image_path,
                          const std::string output_path) {
   auto bmp = std::make_unique<BMP>();
-  if (!bmp->read(image_path)) {
+  if (!bmp->read("../" + image_path)) {
     return false;
   }
   bmp->rotate_image();
-  bmp->write(output_path);
+  bmp->write("../" + output_path);
   return true;
 }
 
 bool ImageEditor::convert_to_grayscale(const std::string image_path,
                                        const std::string output_path) {
   auto bmp = std::make_unique<BMP>();
-  if (!bmp->read(image_path)) {
+  if (!bmp->read("../" + image_path)) {
     return false;
   }
   bmp->convert_to_grayscale();
-  bmp->write(output_path);
+  bmp->write("../" + output_path);
   return true;
 }
