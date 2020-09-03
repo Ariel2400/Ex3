1322a8096e61945e5ec8606fe4c699a180ad4129
Tal Ben-zvi <talbz03@gmail.com>
2020-09-03 18:18:35 +0300

    added cache management classes

--- src/cache_managment/cache_manager.hpp
+++ src/cache_managment/cache_manager.hpp
@@ -2,34 +2,42 @@
 
 class Cache {
 public:
-  // returns true if cache has a file with name file_name_in_cache, else false
+  /* returns true if a file that was created by the described action is in
+    cache, else false */
   static bool is_file_in_cache(const std::string action_description);
 
-  // searches the cache for a file with corresponding name, and prints a message
+  /* searches the cache for a file that was created by the described action, and
+     prints a message */
   static void search(const std::string action_description);
 
-  // prints the file from cache
+  /* prints the file that was created by the described action from cache.
+     the method assumes that the file exists in cache */
   static void print(const std::string action_description);
 
-  // add file to cache
-  static void store_into(const std::string src_file_name,
+  // adds file that was created by the described action to cache
+  static void store_into(const std::string src_file_path,
                          const std::string action_description);
 
-  // retrieve a file from cache
+  // retrieves a file that was created by the described action from cache
   static void load_from(const std::string action_description,
-                        const std::string dest_file_name);
+                        const std::string dest_file_path);
 
   // clears the cache
   static void clear();
 
 private:
+  // creates the file "file_list.txt" in cache if it doesn't already exist
+  static void create_file_list();
 
+  // returns the result file's suffix based on the action described
   static std::string get_suffix(const std::string action_description);
 
-  /* returns the described action's file's serial number in the cash or 0 if
-     it's not there or if an error occurred */
+  /* returns the described action's file's serial number in cash.
+     returns 0 if the file is not in cache.
+     returns -1 if an error occurred */
   static int get_action_serial_number(const std::string action_description);
 
-  // returns the serial number of the next file to be added or 0 if an error occurred
+  /* returns the serial number of the next file to be added
+  returns -1 if an error occurred */
   static int get_next_serial_number();
 };
